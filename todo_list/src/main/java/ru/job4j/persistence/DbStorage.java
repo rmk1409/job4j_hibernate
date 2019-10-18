b9f47ec037fc4344a4fb958633e4d4e5a0beb398
rmk <rmk140987@gmail.com>
2019-10-18 14:11:34 +0300

    2. Лямбды и шаблон wrapper.[#181928]

--- todo_list/src/main/java/ru/job4j/persistence/DbStorage.java
+++ todo_list/src/main/java/ru/job4j/persistence/DbStorage.java
@@ -2,10 +2,12 @@ package ru.job4j.persistence;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import ru.job4j.model.Item;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class DbStorage {
     public SessionFactory factory;
@@ -21,10 +23,9 @@ public class DbStorage {
     }
 
     public Item add(Item item) {
-        Session session = this.factory.openSession();
-        session.save(item);
-        session.beginTransaction().commit();
-        session.close();
+        this.tx(
+                session -> session.save(item)
+        );
         return item;
     }
 
@@ -36,17 +37,30 @@ public class DbStorage {
         return true;
     }
 
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = factory.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
     public List<Item> getAll() {
-        Session session = this.factory.openSession();
-        List<Item> result = session.createQuery("from Item").list();
-        session.close();
-        return result;
+        return this.tx(
+                session -> session.createQuery("from Item").list()
+        );
     }
 
     public List<Item> getAllActual() {
-        Session session = this.factory.openSession();
-        List<Item> result = session.createQuery("from Item where done = false").list();
-        session.close();
-        return result;
+        return this.tx(
+                session -> session.createQuery("from Item where done = false").list()
+        );
     }
 }
